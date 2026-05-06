f8a6a4ae40a8099d6a2d8558f40b9c5e8fb0965b
Matheus Fantini <matheus.fantini01@gmail.com>
2026-05-06 13:17:07 -0300

    correção da criação e conexão com o bando em diferentes plataformas

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'telas/tela_inicial.dart';
+
 import 'database/database_helper.dart';
+import 'telas/tela_inicial.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Necessário para SQLite no Windows/Linux/Mac
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
   await DatabaseHelper.getDatabase();
 
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
